77cc069394a7bdded2ea87a0ecf9bd985823d599
Andreas Rey <anrey@icrc.org>
2021-12-15 22:46:13 +0100

    Refactored data input

--- src/index.js
+++ src/index.js
@@ -1,27 +1,75 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/index.css';
 import _ from './lib/lodash-bundle';
+import $ from 'jquery';
 import setTree from './js/tree';
 import setTable from './js/table';
 import setMap from './js/map';
+import getData from './js/getData';
 
-const dictionaries = require('../data/geometries_results/dictionaries.json');
-const tableRows = require('../data/geometries_results/processings.json');
-const layers = require('../data/geometries_results/layers.json');
-const aoi = require('../data/reference/aoi.json');
-const reference = require('../data/reference/reference.json');
+$('body').show()
 
-setTree(dictionaries);
-
-var headers = [];
-_.each(tableRows[0], function (v, k) {
-  if (['classifier', 'region', 'subregion', 'enddate', 'startdate'].indexOf(k) === -1) {
-    headers.push({ data: k, title: k});
+var hash = window.location.hash
+$.getJSON('./data/list.json', function (list) {
+  if (hash === '') {
+    var displayList = '<select id="processing-selection" class="form-select" aria-label="Processing selection">'
+    _.each(list, function (v, i) {
+      displayList += `
+        <option 
+        ${i === 0 ? 'selected ' : ''}
+        value="${v}">
+        ${v}
+        </option>
+      `
+    })
+    displayList += '</div>'
+    $('#list-anchor').html(displayList);
+    $('#start-button').on('click', function () {
+      var processingTitle = $('#processing-selection').val()
+      window.location.hash = processingTitle
+      access(processingTitle)
+    })
+    $('#home').show();
+    $('#loading').hide();
+  } else {
+    var hashValue = _.replace(hash, '#', '')
+    if (list.indexOf(hashValue) !== -1) {
+      access(hashValue)
+    } else {
+      window.location.href = window.location.origin
+    }
   }
 })
-setTable(tableRows, headers);
-setMap({
-  layers,
-  aoi,
-  reference
-});
+  
+
+
+function access (processingTitle) {
+  $('#loading-modal-title').html('Retrieving processing results')
+  $('#loading').show();
+  $('#home').hide();
+  getData(processingTitle, function (data) {
+
+    var dictionaries = data.dictionaries
+    var tableRows = data.processings
+    var layers = data.layers
+    var aoi = data.aoi
+    var reference = data.reference
+
+    setTree(dictionaries);
+
+    var headers = [];
+    _.each(tableRows[0], function (v, k) {
+      if (['classifier', 'region', 'enddate', 'startdate'].indexOf(k) === -1) {
+        headers.push({ data: k, title: k});
+      }
+    })
+    $('#loading').hide();
+    $('#presentation').show();
+    setTable(tableRows, headers);
+    setMap({
+      layers,
+      aoi,
+      reference
+    });
+  })
+}
---
+++ src/js/getData.js
@@ -0,0 +1,30 @@
+import $ from 'jquery'
+import _ from '../lib/lodash-bundle'
+
+var processed = ['dictionaries', 'processings', 'layers']
+var reference = ['aoi', 'reference']
+
+function getData (folder, callback) {
+
+  var data = {}
+  var remainingSources = 5
+
+  function getJSON (key, url) {
+    $.getJSON(url, function (jsonData) {
+      data[key] = jsonData
+      remainingSources--
+      if (remainingSources === 0) {
+        callback(data)
+      }
+    })
+  }
+
+  _.each(processed, function (v) {
+    getJSON(v, `./data/${folder}/results/${v}.json`, callback)
+  })
+  _.each(reference, function (v) {
+    getJSON(v, `./data/${folder}/reference/${v}.json`, callback)
+  })
+}
+
+export default getData
