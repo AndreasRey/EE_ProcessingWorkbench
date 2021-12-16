bcdfd7a672722652a6b72ade99e2d8697dd31db5
Andreas Rey <anrey@icrc.org>
2021-12-16 03:46:42 +0100

    New processings integrated

---
+++ src/integration/3_Single_PolygonsWithErrors.js
@@ -0,0 +1,13 @@
+// Run this file using the "node" command.
+var integrateExports = require("../scripts/integrateExports");
+
+var processingName = '3_Single_PolygonsWithErrors'
+var suffix = '3'
+
+var source_AoI = `${processingName}_AoI_export_GeoJSON_${suffix}.geojson`
+var source_ESA = `${processingName}_ESA_export_GeoJSON_${suffix}.geojson`
+var source_dictionnaries = `${processingName}_dictionaries_geoJSON_${suffix}.geojson`
+var source_data = `${processingName}_GeoJSON_${suffix}.geojson`
+var outputFolder = `0${processingName}`
+
+integrateExports(source_AoI, source_ESA, source_dictionnaries, source_data, outputFolder);
