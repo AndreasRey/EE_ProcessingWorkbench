eefd774c9cec6171f68f3171d7c694e29130dc4f
Andreas Rey <anrey@icrc.org>
2022-01-11 22:52:18 +0100

    added lodash package

--- src/lib/lodash-bundle.js
+++ src/lib/lodash-bundle.js
@@ -3,11 +3,13 @@ import each from "lodash.forEach";
 import cloneDeep from "lodash.clonedeep";
 import endsWith from "lodash.endswith";
 import replace from "lodash.replace";
+import startsWith from "lodash.startswith";
 
 export default {
   cloneDeep,
   each,
   endsWith,
   keyBy,
-  replace
+  replace,
+  startsWith
 }
